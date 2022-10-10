efd71169b916d0368d67785167bc3094285b69c2
Afumoons <afumoons@gmail.com>
2022-10-10 15:30:11 +0700

    Add eloquent relationship chatMessages() on ChatRoom

--- app/Models/ChatRoom.php
+++ app/Models/ChatRoom.php
@@ -12,4 +12,10 @@ class ChatRoom extends Model
     protected $fillable = [
         'name',
     ];
+
+    // hasMany relationship with ChatMessage
+    public function chatMessages()
+    {
+        return $this->hasMany(ChatMessage::class, 'chat_room_id', 'id');
+    }
 }
